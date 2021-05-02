045ee1dc95241db5e97223b262cda2277ce18b84
Min Shin Khant <minshinkhant7@gmail.com>
2021-05-01 23:54:04 -0400

    added state2 update_2

--- state2/src/App.js
+++ state2/src/App.js
@@ -1,22 +1,50 @@
 import React, { useState } from 'react';
 
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        This app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = (props) => {
+  console.log("props", props)
+  return(
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+  )
+  }
+
 const App = () => {
   const [clicks, setClicks] = useState({left: 0, right:0})
+  const [allClicks, setAll] = useState([])
 
-  const handleLeftClick = () => setClicks({...clicks, left:clicks.left + 1})
-
-  const handleRightClick = () => setClicks({...clicks, right:clicks.right + 1})
-
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setClicks({...clicks, left:clicks.left + 1})
+  }
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setClicks({...clicks, right:clicks.right + 1})
+  }
+  //debugger
   return (
     <div>
       {clicks.left}
-      <button onClick={handleLeftClick}>
-        left
-      </button>
-      <button onClick={handleRightClick}>
-        right
-      </button>
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right'/>
       {clicks.right}
+      <History allClicks={allClicks}/>
     </div>
   )
 }
